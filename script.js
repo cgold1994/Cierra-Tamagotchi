e31df707b191ca954a6426b56c4711cbbb41e27d
Goldsmith <cierra.goldsmith@pearson.com>
2022-03-06 15:54:24 -0500

    working on intervals

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// document.getElementById("myButton").onclick = function(){
-//     let myName = document.getElementById("myText").value;
-//     console.log("Hi",myName);
-// }
-
-
 class Tamagotchi {
     constructor(hunger, sleepiness, boredom, age) {
         this.hunger = hunger;
@@ -15,12 +9,68 @@ class Tamagotchi {
 
 // $("#restart").hide();
 
-function startGame() {
-    grow();
+// function startGame() {
+//     grow();
+// }
+
+
+
+let count = 0;
+let myCounter = null;
+
+
+const sleepCount = document.getElementById("countSleep")
+const hungerCount = document.getElementById("countHunger")
+const boredomCount = document.getElementById("countBoredom")
+
+const buttonSleep = document.getElementById("sleepBtn")
+const buttonHunger = document.getElementById("feedBtn")
+const buttonBoredom = document.getElementById("playBtn")
+
+
+
+
+const element = document.getElementById("myButton");
+      element.addEventListener("click", myFunction);
+      
+      function myFunction() {
+        document.getElementById("demo").innerHTML = "Hey! I'm so happy that you're here to<br> keep me company.<br>In order to keep me happy and alive, try to keep my action levels at 10.";
+        myCounter = setInterval(function(){
+            count++;
+            stopCount()
+            sleepCount.innerText = "Sleepiness " + count;
+            hungerCount.innerText = "Hunger" + count;
+            boredomCount.innerText = "Boredom" + count;
+          }, 1000);
+        }
+
+function gotosleep () {
+
 }
 
-function grow() {
-    if (myTamagotchi.age < 1) {
-        appear();
-    }
+function icecream () {
+
 }
+
+function catnip () {
+
+}
+
+function stopCount() {
+    if (count >= 10){
+      clearInterval(myCounter)
+    }
+    else {
+        return
+    }
+  }
+
+
+
+
+
+buttonSleep.addEventListener("click", gotosleep);
+buttonHunger.addEventListener("click", icecream);
+buttonBoredom.addEventListener("click", catnip);
+
+
